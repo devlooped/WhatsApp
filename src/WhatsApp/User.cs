4803b854742966455278e705f87fc8b3b3f7d7c3
Daniel Cazzulino <daniel@cazzulino.com>
2025-05-05 11:10:26 -0300

    Normalize user's numbers automatically on every payload
    This makes it easier to just Reply to a message or Send without having to normalize anything.

--- src/WhatsApp/User.cs
+++ src/WhatsApp/User.cs
@@ -3,6 +3,31 @@
 /// <summary>
 /// WhatsApp end user that either originated a message or is the target of a message.
 /// </summary>
-/// <param name="Name">User's name</param>
-/// <param name="Number">User's phone number</param>
-public record User(string Name, string Number);
+public record User
+{
+    /// <summary>
+    /// User's name.
+    /// </summary>
+    public string Name { get; init; }
+
+    /// <summary>
+    /// User's phone number (normalized).
+    /// </summary>
+    public string Number { get; init; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="User"/> class.
+    /// </summary>
+    /// <param name="name">User's name.</param>
+    /// <param name="number">Users's number.</param>
+    public User(string name, string number)
+    {
+        Name = name;
+        Number = NormalizeNumber(number);
+    }
+
+    static string NormalizeNumber(string number) =>
+        // On the web, we don't get the 9 after 54 \o/
+        // so for Argentina numbers, we need to remove the 9.
+        number.StartsWith("549", StringComparison.Ordinal) ? "54" + number[3..] : number;
+}
