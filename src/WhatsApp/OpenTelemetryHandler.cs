b496df11c872ac28334d44b413217c12be0892aa
Daniel Cazzulino <daniel@cazzulino.com>
2025-05-16 11:15:20 -0300

    WIP: OpenTelemetry

---
+++ src/WhatsApp/OpenTelemetryHandler.cs
@@ -0,0 +1,27 @@
+﻿namespace Devlooped.WhatsApp;
+
+/// <summary>
+/// Represents a delegating handler that implements the OpenTelemetry 
+/// Semantic Conventions for Messaging systems.
+/// </summary>
+/// <remarks>
+/// This class provides an implementation of applicable Semantic Conventions for 
+/// Messaging systems v1.33, defined at <see href="https://opentelemetry.io/docs/specs/semconv/messaging/" />.
+/// </remarks>
+public class OpenTelemetryHandler(IWhatsAppHandler innerHandler) : DelegatingHandler(innerHandler)
+{
+    /// <summary>
+    /// Gets or sets a value indicating whether potentially sensitive information 
+    /// should be included in telemetry.
+    /// </summary>
+    /// <value>
+    /// <see langword="true"/> if potentially sensitive information should be included in telemetry;
+    /// <see langword="false"/> if telemetry shouldn't include raw inputs.
+    /// The default value is <see langword="false"/>.
+    /// </value>
+    /// <remarks>
+    /// By default, telemetry includes metadata, such as invocation counts, but not raw inputs, 
+    /// such as message content.
+    /// </remarks>
+    public bool EnableSensitiveData { get; set; }
+}
---
+++ src/WhatsApp/OpenTelemetryHandlerExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+
+namespace Devlooped.WhatsApp;
+
+/// <summary>
+/// Provides extensions for configuring <see cref="OpenTelemetryHandler"/> instances.
+/// </summary>
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static class OpenTelemetryHandlerExtensions
+{
+    /// <summary>
+    /// Adds the <see cref="OpenTelemetryHandler"/> handler to the pipeline.
+    /// </summary>
+    /// <param name="builder">The handler builder.</param>
+    /// <param name="loggerFactory">The logger factory.</param>
+    /// <returns>The updated handler builder.</returns>
+    public static WhatsAppHandlerBuilder UseOpenTelemetry(
+        this WhatsAppHandlerBuilder builder,
+        Action<OpenTelemetryHandler>? configure = null)
+    {
+        _ = Throw.IfNull(builder);
+
+        return builder.Use((inner, services) =>
+        {
+            var handler = new OpenTelemetryHandler(inner);
+            configure?.Invoke(handler);
+            return handler;
+        });
+    }
+}
