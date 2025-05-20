4395abb4a409d4694c2252f680dcd16107bf462c
Daniel Cazzulino <daniel@cazzulino.com>
2025-05-20 16:20:41 -0300

    Add an AsBuilder extension method for improved discoverability

--- src/WhatsApp/WhatsAppHandlerExtensions.cs
+++ src/WhatsApp/WhatsAppHandlerExtensions.cs
@@ -1,5 +1,21 @@
 ﻿namespace Devlooped.WhatsApp;
 
-class WhatsAppHandlerExtensions
+/// <summary>
+/// Provides the <see cref="AsBuilder"/> extension method to build a pipeline 
+/// around a given handler.
+/// </summary>
+public static class WhatsAppHandlerExtensions
 {
+    /// <summary>
+    /// Creates a new <see cref="WhatsAppHandlerBuilder"/> using <paramref name="handler"/> as its inner handler.
+    /// </summary>
+    /// <remarks>
+    /// This method is equivalent to using the <see cref="WhatsAppHandlerBuilder"/> constructor directly.
+    /// </remarks>
+    /// <exception cref="ArgumentNullException"><paramref name="handler"/> is <see langword="null"/>.</exception>
+    public static WhatsAppHandlerBuilder AsBuilder(this IWhatsAppHandler handler)
+    {
+        Throw.IfNull(handler);
+        return new(_ => handler);
+    }
 }
