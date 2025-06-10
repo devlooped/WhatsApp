7d950f0ba33f9583f338d92007ddf0cdedf33c92
Daniel Cazzulino <daniel@cazzulino.com>
2025-06-10 10:18:50 -0300

    Fix hang on main handler registration, cleanup sample handler (#115)
    * Fixed hang by avoiding registering the business handler with IWhatsAppHandler
    
    IWhatsAppHandler is already registered by the builder to the the entire pipeline.
    
    So, we should try to resolve the provided concrete handler type and resolve all the dependencies defined in the ctor.
    
    * Moved out the main process handler out of the program.cs
    
    ---------
    
    Co-authored-by: Adrian Alonso <adalonso@microsoft.com>

---
+++ src/SampleApp/Sample/ProcessHandler.cs
@@ -0,0 +1,59 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text.Json;
+using Devlooped.WhatsApp;
+using Microsoft.Extensions.Logging;
+
+class ProcessHandler(ILogger<Program> logger, JsonSerializerOptions options) : IWhatsAppHandler
+{
+    public async IAsyncEnumerable<Response> HandleAsync(IEnumerable<IMessage> messages, [EnumeratorCancellation] CancellationToken cancellation = default)
+    {
+        // Avoid warning CS1998 // Async method lacks 'await' operators and will run synchronously
+        await Task.CompletedTask;
+
+        var message = messages.Last();
+        logger.LogInformation("💬 Received message: {Message}", message);
+
+        if (message is ErrorMessage error)
+        {
+            // Reengagement error, we need to invite the user.
+            if (error.Error.Code == 131047)
+            {
+                // Showcases how to use a pre-declared template response to reengage the user.
+                yield return error.Template("reengagement", "es_AR");
+            }
+            else
+            {
+                logger.LogWarning("⚠️ Unknown error message received: {Error}", message);
+            }
+        }
+        else if (message is InteractiveMessage interactive)
+        {
+            logger.LogWarning("👤 chose {Button} ({Title})", interactive.Button.Id, interactive.Button.Title);
+            yield return interactive.Reply($"👤 chose: {interactive.Button.Title} ({interactive.Button.Id})");
+        }
+        else if (message is ReactionMessage reaction)
+        {
+            logger.LogInformation("👤 reaction: {Reaction}", reaction.Emoji);
+            yield return reaction.Reply($"👤 reaction: {reaction.Emoji}");
+        }
+        else if (message is StatusMessage status)
+        {
+            logger.LogInformation("☑️ status: {Status}", status.Status);
+        }
+        else if (message is ContentMessage content)
+        {
+            yield return content.React("🧠");
+
+            // simulate some hard work at hand, like doing some LLM-stuff :)
+            //await Task.Delay(2000);
+            yield return content.Reply(
+                $"☑️ Got your {content.Content.Type}:\r\n{JsonSerializer.Serialize(content, options)}",
+                new Button("btn_good", "👍"),
+                new Button("btn_bad", "👎"));
+        }
+        else if (message is UnsupportedMessage unsupported)
+        {
+            logger.LogWarning("⚠️ {Message}", unsupported);
+        }
+    }
+}
--- src/SampleApp/Sample/Program.cs
+++ src/SampleApp/Sample/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Runtime.CompilerServices;
-using System.Text.Json;
+﻿using System.Text.Json;
 using System.Text.Json.Serialization;
 using Devlooped;
 using Devlooped.WhatsApp;
@@ -42,7 +41,7 @@ builder.Services.AddSingleton(services => builder.Environment.IsDevelopment() ?
     throw new InvalidOperationException("Missing required App:Storage connection string."));
 
 builder.Services
-    .AddWhatsApp<ILogger<Program>, JsonSerializerOptions>(builder.Configuration, ProcessMessagesAsync)
+    .AddWhatsApp<ProcessHandler>(builder.Configuration)
     // Matches what we use in ConfigureOpenTelemetry
     .UseOpenTelemetry(builder.Environment.ApplicationName)
     .UseLogging()
@@ -50,59 +49,3 @@ builder.Services
     .UseConversation();
 
 builder.Build().Run();
-
-static async IAsyncEnumerable<Response> ProcessMessagesAsync(
-    ILogger<Program> logger,
-    JsonSerializerOptions options,
-    IEnumerable<IMessage> messages,
-    [EnumeratorCancellation] CancellationToken cancellationToken)
-{
-    // Avoid warning CS1998 // Async method lacks 'await' operators and will run synchronously
-    await Task.CompletedTask;
-
-    var message = messages.Last();
-    logger.LogInformation("💬 Received message: {Message}", message);
-
-    if (message is ErrorMessage error)
-    {
-        // Reengagement error, we need to invite the user.
-        if (error.Error.Code == 131047)
-        {
-            // Showcases how to use a pre-declared template response to reengage the user.
-            yield return error.Template("reengagement", "es_AR");
-        }
-        else
-        {
-            logger.LogWarning("⚠️ Unknown error message received: {Error}", message);
-        }
-    }
-    else if (message is InteractiveMessage interactive)
-    {
-        logger.LogWarning("👤 chose {Button} ({Title})", interactive.Button.Id, interactive.Button.Title);
-        yield return interactive.Reply($"👤 chose: {interactive.Button.Title} ({interactive.Button.Id})");
-    }
-    else if (message is ReactionMessage reaction)
-    {
-        logger.LogInformation("👤 reaction: {Reaction}", reaction.Emoji);
-        yield return reaction.Reply($"👤 reaction: {reaction.Emoji}");
-    }
-    else if (message is StatusMessage status)
-    {
-        logger.LogInformation("☑️ status: {Status}", status.Status);
-    }
-    else if (message is ContentMessage content)
-    {
-        yield return content.React("🧠");
-
-        // simulate some hard work at hand, like doing some LLM-stuff :)
-        //await Task.Delay(2000);
-        yield return content.Reply(
-            $"☑️ Got your {content.Content.Type}:\r\n{JsonSerializer.Serialize(content, options)}",
-            new Button("btn_good", "👍"),
-            new Button("btn_bad", "👎"));
-    }
-    else if (message is UnsupportedMessage unsupported)
-    {
-        logger.LogWarning("⚠️ {Message}", unsupported);
-    }
-}
