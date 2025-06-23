2f15f8b6a22ef7368f28659acd7553d43c34bba2
Daniel Cazzulino <daniel@cazzulino.com>
2025-06-23 01:00:50 -0300

    Allow flexible mark as read behavior for content messages
    You can set ReadOnMessage or ReadOnProcess to determine when/if user's messages will be marked read.

--- src/WhatsApp/WhatsAppOptions.cs
+++ src/WhatsApp/WhatsAppOptions.cs
@@ -6,6 +6,16 @@
 public class WhatsAppOptions
 {
     /// <summary>
+    /// Mark messages as read when received in the WhatsApp webhook endpoint.
+    /// </summary>
+    public bool? ReadOnMessage { get; set; }
+
+    /// <summary>
+    /// Mark messages as read when processing is started.
+    /// </summary>
+    public bool? ReadOnProcess { get; set; }
+
+    /// <summary>
     /// An optional emoji to react with when a message is received 
     /// in the WhatsApp webhook endpoint.
     /// </summary>
