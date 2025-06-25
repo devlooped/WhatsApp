7fef8c3b2e2927530e10e48dea19e5460c554c19
Daniel Cazzulino <daniel@cazzulino.com>
2025-06-25 12:38:16 -0300

    Add a way to skip a handler in the pipeline
    When building the pipeline, sometimes it's useful for Use(...) to conditionally turn themselves off depending on some service or configuration. Rather than inject a passthrough DelegatingHandler needlessly, this allows these cases to return a WhatsAppHandler.Skip instead, which will not be added to the pipeline at all

--- src/WhatsApp/WhatsAppHandler.cs
+++ src/WhatsApp/WhatsAppHandler.cs
@@ -6,13 +6,28 @@
 public static class WhatsAppHandler
 {
     /// <summary>
-    /// An empty implementation of <see cref="IWhatsAppHandler"/> that does nothing.
+    /// An empty implementation of <see cref="IWhatsAppHandler"/> that does nothing and 
+    /// can be used to shortcircuit the processing of WhatsApp messages.
     /// </summary>
     public static IWhatsAppHandler Empty { get; } = new EmptyWhatsAppHandler();
+
+    /// <summary>
+    /// An empty implementation of <see cref="IWhatsAppHandler"/> that is skipped 
+    /// when building the processing pipeline. It's useful to implement conditional 
+    /// <c>Use(..)</c> logic that is dependent on runtime conditions, such as the 
+    /// hosting environment or configuration settings.
+    /// </summary>
+    public static IWhatsAppHandler Skip { get; } = new SKipWhatsAppHandler();
 
     class EmptyWhatsAppHandler : IWhatsAppHandler
     {
         public IAsyncEnumerable<Response> HandleAsync(IEnumerable<IMessage> messages, CancellationToken cancellation = default)
             => AsyncEnumerable.Empty<Response>();
+    }
+
+    class SKipWhatsAppHandler : IWhatsAppHandler
+    {
+        public IAsyncEnumerable<Response> HandleAsync(IEnumerable<IMessage> messages, CancellationToken cancellation = default)
+            => throw new NotSupportedException("This handler should never be invoked by the pipeline.");
     }
 }
