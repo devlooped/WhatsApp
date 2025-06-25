a4212a0c74d78878257d51dec423c80c6db721b9
Daniel Cazzulino <daniel@cazzulino.com>
2025-06-25 16:11:45 -0300

    Rename handlers Empty > Stop, Skip > Continue
    These names reflect much better what's the pipeline behavior when it encounters them:
    
    * Stop: causes the pipeline to stop further processing and aborting any response generation.
    * Continue: continues execution as if the handler wasn't even present in the pipeline (which it isn't :)).
    
    Both allow runtime-conditional behaviors when used in combination with `Use(...)`.

--- src/WhatsApp/WhatsAppHandler.cs
+++ src/WhatsApp/WhatsAppHandler.cs
@@ -6,26 +6,28 @@
 public static class WhatsAppHandler
 {
     /// <summary>
-    /// An empty implementation of <see cref="IWhatsAppHandler"/> that does nothing and 
-    /// can be used to shortcircuit the processing of WhatsApp messages.
+    /// An empty implementation of <see cref="IWhatsAppHandler"/> that is skipped 
+    /// when building the processing pipeline so that normal processing continues 
+    /// as if this handler was never added at all. It's useful to implement conditional 
+    /// <c>Use(..)</c> logic that is dependent on runtime conditions, such as the 
+    /// hosting environment or configuration settings, with zero impact on runtime 
+    /// when the condition is not met.
     /// </summary>
-    public static IWhatsAppHandler Empty { get; } = new EmptyWhatsAppHandler();
+    public static IWhatsAppHandler Continue { get; } = new ContinueWhatsAppHandler();
 
     /// <summary>
-    /// An empty implementation of <see cref="IWhatsAppHandler"/> that is skipped 
-    /// when building the processing pipeline. It's useful to implement conditional 
-    /// <c>Use(..)</c> logic that is dependent on runtime conditions, such as the 
-    /// hosting environment or configuration settings.
+    /// An empty implementation of <see cref="IWhatsAppHandler"/> that stops further 
+    /// execution of the pipeline and generates no responses.
     /// </summary>
-    public static IWhatsAppHandler Skip { get; } = new SKipWhatsAppHandler();
+    public static IWhatsAppHandler Stop { get; } = new StopWhatsAppHandler();
 
-    class EmptyWhatsAppHandler : IWhatsAppHandler
+    class StopWhatsAppHandler : IWhatsAppHandler
     {
         public IAsyncEnumerable<Response> HandleAsync(IEnumerable<IMessage> messages, CancellationToken cancellation = default)
             => AsyncEnumerable.Empty<Response>();
     }
 
-    class SKipWhatsAppHandler : IWhatsAppHandler
+    class ContinueWhatsAppHandler : IWhatsAppHandler
     {
         public IAsyncEnumerable<Response> HandleAsync(IEnumerable<IMessage> messages, CancellationToken cancellation = default)
             => throw new NotSupportedException("This handler should never be invoked by the pipeline.");
