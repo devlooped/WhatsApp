c9498dd70ce4889c00fc141363aa757de8cc98ca
Daniel Cazzulino <daniel@cazzulino.com>
2025-07-08 18:43:35 -0300

    Remove dependency on System.Linq.Async
    This can confuse the compiler under .NET10 (preview5 at least)

---
+++ src/WhatsApp/Extensions/AsyncEnumerable.cs
@@ -0,0 +1,25 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+static partial class AsyncEnumerable
+{
+    /// <summary>
+    /// Returns an empty <see cref="IAsyncEnumerable{T}"/> that has the specified type argument.
+    /// </summary>
+    /// <typeparam name="TResult">The type of the elements of the sequence.</typeparam>
+    /// <returns>An empty <see cref="IAsyncEnumerable{T}"/> whose type argument is <typeparamref name="TResult"/>.</returns>
+    public static IAsyncEnumerable<TResult> Empty<TResult>() => EmptyAsyncEnumerable<TResult>.Instance;
+
+    sealed class EmptyAsyncEnumerable<TResult> : IAsyncEnumerable<TResult>, IAsyncEnumerator<TResult>
+    {
+        public static readonly EmptyAsyncEnumerable<TResult> Instance = new();
+
+        public IAsyncEnumerator<TResult> GetAsyncEnumerator(CancellationToken cancellationToken = default) => this;
+
+        public ValueTask<bool> MoveNextAsync() => default;
+
+        public TResult Current => default!;
+
+        public ValueTask DisposeAsync() => default;
+    }
+}
