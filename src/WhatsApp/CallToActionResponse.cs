841ac63fa4a5379c864e153e1bcece93cfc48e10
Daniel Cazzulino <daniel@cazzulino.com>
2025-07-09 12:39:18 -0300

    Minor CallToAction cleanup
    - SendXXX is used when there isn't a clear verb we can use. In this case, CallToAction is already a verb/action so we don't need the Send prefix (just like Reply/React).
    - Calls to action can (for now?) never be a reply to anything, they are standalone messages. Remove the Context everywhere therefore, since we don't use it at all anyway in the actual sending implementation.
    - Unify naming on Action for the button text.

--- src/WhatsApp/CallToActionResponse.cs
+++ src/WhatsApp/CallToActionResponse.cs
@@ -4,19 +4,25 @@
 /// Represents an interactive call to action that can be sent in response to a user message.
 /// </summary>
 /// <see cref="https://developers.facebook.com/docs/whatsapp/cloud-api/messages/interactive-cta-url-messages/"/>
-public record CallToActionResponse(string ServiceId, string UserNumber, string? Context, string Text, string ButtonText, string Url, string? ConversationId) : Response(ServiceId, UserNumber, Context, ConversationId)
+/// <param name="ServiceId">The identifier of the service handling the message.</param>
+/// <param name="UserNumber">The phone number of the recipient in international format.</param>
+/// <param name="Text">The content of the message calling to action.</param>
+/// <param name="Action">The action button text.</param>
+/// <param name="Url">The URL to navigate to when the action button is clicked.</param>
+/// /// <param name="ConversationId">The conversation id where this response was generated</param>
+public record CallToActionResponse(string ServiceId, string UserNumber, string Text, string Action, string Url, string? ConversationId) : Response(ServiceId, UserNumber, null, ConversationId)
 {
     readonly CompositeService? service;
 
-    internal CallToActionResponse(Service service, string userNumber, string? context, string Text, string ButtonText, string Url, string? conversationId)
-        : this(service.Id, userNumber, context, Text, ButtonText, Url, conversationId)
+    internal CallToActionResponse(Service service, string userNumber, string Text, string Action, string Url, string? conversationId)
+        : this(service.Id, userNumber, Text, Action, Url, conversationId)
         => this.service = service as CompositeService;
 
     protected override Task<string?> SendCoreAsync(IWhatsAppClient client, CancellationToken cancellation = default)
     {
         if (service != null)
-            return client.SendCallToActionAsync(service.Secondary.Id, UserNumber, Text, ButtonText, Url, cancellation);
+            return client.CallToActionAsync(service.Secondary.Id, UserNumber, Text, Action, Url, cancellation);
 
-        return client.SendCallToActionAsync(ServiceId, UserNumber, Text, ButtonText, Url, cancellation);
+        return client.CallToActionAsync(ServiceId, UserNumber, Text, Action, Url, cancellation);
     }
 }
