d6003814697aca4d843049fdfea3ad24bfd27456
Daniel Cazzulino <daniel@cazzulino.com>
2025-12-18 18:56:06 -0300

    Bump dependencies to latest stable for release
    Update also the Aspire sample app

--- src/SampleApp/Dashboard/Program.cs
+++ src/SampleApp/Dashboard/Program.cs
@@ -1,12 +1,10 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+// Run Azurite via npx instead of container
+var storage = builder.AddExecutable("azurite", "npx", ".", "azurite", "--silent", "--location", ".azurite", "--debug", ".azurite/debug.log");
+
 builder.AddAzureFunctionsProject<Projects.Sample>("api")
-       // Functions App will launch with default port, not the launchSettings.json port
-       // So we request a proxy to forward 4242 to the default function app port instead.
-       // This will only work if you have a single Function App.
-       // See https://github.com/dotnet/aspire/issues/8589
-       //.WithArgs("--port", "4242")
-       .WithHttpEndpoint(4242, 7071, "apiproxy")
+       .WaitFor(storage)
        .WithEnvironment("AzureWebJobsStorage", "UseDevelopmentStorage=true")
        // Disable Azure SDK telemetry since we're running locally
        .WithEnvironment("AZURE_SDK_TELEMETRY_ENABLED", "false")
