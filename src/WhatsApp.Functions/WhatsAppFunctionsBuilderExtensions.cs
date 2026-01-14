bc468976ab2e9ed491cc19100d95183fee99d832
Daniel Cazzulino <daniel@cazzulino.com>
2026-01-14 16:38:36 -0300

    Make the existing functions AddWhatsApp legacy and hidden
    The new functions builder extension methods make it more intuitive and less error-prone

--- src/WhatsApp.Functions/WhatsAppFunctionsBuilderExtensions.cs
+++ src/WhatsApp.Functions/WhatsAppFunctionsBuilderExtensions.cs
@@ -1,5 +1,9 @@
 ﻿using System.ComponentModel;
+using Azure.Storage.Queues;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Devlooped.WhatsApp;
 
@@ -7,142 +11,133 @@ namespace Devlooped.WhatsApp;
 /// Provides extension methods for registering WhatsApp services for Azure Functions.
 /// </summary>
 [EditorBrowsable(EditorBrowsableState.Never)]
-public static class WhatsAppFunctionsServiceCollectionExtensions
+public static class WhatsAppFunctionsBuilderExtensions
 {
     /// <summary>Registers a singleton <see cref="IWhatsAppClient"/> and <see cref="IWhatsAppHandler"/> in the <see cref="IServiceCollection"/> for Azure Functions hosting.</summary>
-    /// <param name="collection">The <see cref="IServiceCollection"/> to which the client should be added.</param>
+    /// <param name="builder">The <see cref="IFunctionsWorkerApplicationBuilder"/> to which the WhatsApp services should be added.</param>
     /// <param name="handler">The <see cref="IWhatsAppHandler"/> that handles incoming WhatsApp messages as the underlying backend.</param>
     /// <param name="lifetime">The service lifetime for the client and handler. Defaults to <see cref="ServiceLifetime.Singleton"/>.</param>
     /// <param name="configure">Optional configuration callback for <see cref="WhatsAppOptions"/>.</param>
     /// <returns>A <see cref="WhatsAppHandlerBuilder"/> that can be used to build a pipeline around the handler.</returns>
-    [EditorBrowsable(EditorBrowsableState.Never)]
     public static WhatsAppHandlerBuilder AddWhatsApp(
-        this IServiceCollection collection,
+        this IFunctionsWorkerApplicationBuilder builder,
         IWhatsAppHandler handler,
         ServiceLifetime lifetime = ServiceLifetime.Singleton,
         Action<WhatsAppOptions>? configure = null)
     {
-        var builder = collection.AddWhatsAppCore(handler, lifetime, configure);
-        collection.ConfigureAzureFunctions(builder);
-        return builder;
+        builder.UseFunctionContextAccessor();
+        var handlerBuilder = builder.Services.AddWhatsAppCore(handler, lifetime, configure);
+        return ConfigureAzureFunctions(builder.Services, handlerBuilder);
     }
 
     /// <summary>Registers a singleton <see cref="IWhatsAppClient"/> and <see cref="IWhatsAppHandler"/> in the <see cref="IServiceCollection"/> for Azure Functions hosting.</summary>
-    /// <param name="collection">The <see cref="IServiceCollection"/> to which the client should be added.</param>
+    /// <param name="builder">The <see cref="IFunctionsWorkerApplicationBuilder"/> to which the WhatsApp services should be added.</param>
     /// <param name="handlerFactory">A callback that produces the inner <see cref="IWhatsAppHandler"/> that represents the underlying backend.</param>
     /// <param name="lifetime">The service lifetime for the client and handler. Defaults to <see cref="ServiceLifetime.Singleton"/>.</param>
     /// <param name="configure">Optional configuration callback for <see cref="WhatsAppOptions"/>.</param>
     /// <returns>A <see cref="WhatsAppHandlerBuilder"/> that can be used to build a pipeline around the handler.</returns>
-    [EditorBrowsable(EditorBrowsableState.Never)]
     public static WhatsAppHandlerBuilder AddWhatsApp(
-        this IServiceCollection collection,
+        this IFunctionsWorkerApplicationBuilder builder,
         Func<IServiceProvider, IWhatsAppHandler> handlerFactory,
         ServiceLifetime lifetime = ServiceLifetime.Singleton,
         Action<WhatsAppOptions>? configure = null)
     {
-        var builder = collection.AddWhatsAppCore(handlerFactory, lifetime, configure);
-        collection.ConfigureAzureFunctions(builder);
-        return builder;
+        builder.UseFunctionContextAccessor();
+        var handlerBuilder = builder.Services.AddWhatsAppCore(handlerFactory, lifetime, configure);
+        return ConfigureAzureFunctions(builder.Services, handlerBuilder);
     }
 
     /// <summary>
     /// Add WhatsApp services for Azure Functions and use an already registered service that implements <see cref="IWhatsAppHandler"/>.
     /// </summary>
-    [EditorBrowsable(EditorBrowsableState.Never)]
     public static WhatsAppHandlerBuilder AddWhatsApp(
-        this IServiceCollection collection,
+        this IFunctionsWorkerApplicationBuilder builder,
         ServiceLifetime lifetime = ServiceLifetime.Singleton,
         Action<WhatsAppOptions>? configure = null)
     {
-        var builder = collection.AddWhatsAppCore(lifetime, configure);
-        collection.ConfigureAzureFunctions(builder);
-        return builder;
+        builder.UseFunctionContextAccessor();
+        var handlerBuilder = builder.Services.AddWhatsAppCore(lifetime, configure);
+        return ConfigureAzureFunctions(builder.Services, handlerBuilder);
     }
 
     /// <summary>
     /// Configure the WhatsApp handler with a typed handler for Azure Functions hosting.
     /// </summary>
-    [EditorBrowsable(EditorBrowsableState.Never)]
     public static WhatsAppHandlerBuilder AddWhatsApp<THandler>(
-        this IServiceCollection collection,
+        this IFunctionsWorkerApplicationBuilder builder,
         ServiceLifetime lifetime = ServiceLifetime.Singleton,
         Action<WhatsAppOptions>? configure = null)
         where THandler : class, IWhatsAppHandler
     {
-        var builder = collection.AddWhatsAppCore<THandler>(lifetime, configure);
-        collection.ConfigureAzureFunctions(builder);
-        return builder;
+        builder.UseFunctionContextAccessor();
+        var handlerBuilder = builder.Services.AddWhatsAppCore<THandler>(lifetime, configure);
+        return ConfigureAzureFunctions(builder.Services, handlerBuilder);
     }
 
     /// <summary>
     /// Configure the WhatsApp handler with an anonymous handler delegate for Azure Functions hosting.
     /// </summary>
-    [EditorBrowsable(EditorBrowsableState.Never)]
     public static WhatsAppHandlerBuilder AddWhatsApp(
-        this IServiceCollection collection,
+        this IFunctionsWorkerApplicationBuilder builder,
         Func<IServiceProvider, IEnumerable<IMessage>, CancellationToken, IAsyncEnumerable<Response>> handler,
         ServiceLifetime lifetime = ServiceLifetime.Singleton,
         Action<WhatsAppOptions>? configure = null)
     {
-        var builder = collection.AddWhatsAppCore(handler, lifetime, configure);
-        collection.ConfigureAzureFunctions(builder);
-        return builder;
+        builder.UseFunctionContextAccessor();
+        var handlerBuilder = builder.Services.AddWhatsAppCore(handler, lifetime, configure);
+        return ConfigureAzureFunctions(builder.Services, handlerBuilder);
     }
 
     /// <summary>
     /// Configure the WhatsApp handler with an anonymous handler delegate for Azure Functions hosting.
     /// </summary>
-    [EditorBrowsable(EditorBrowsableState.Never)]
     public static WhatsAppHandlerBuilder AddWhatsApp(
-        this IServiceCollection collection,
+        this IFunctionsWorkerApplicationBuilder builder,
         Func<IEnumerable<IMessage>, CancellationToken, IAsyncEnumerable<Response>> handler,
         ServiceLifetime lifetime = ServiceLifetime.Singleton,
         Action<WhatsAppOptions>? configure = null)
     {
-        var builder = collection.AddWhatsAppCore(handler, lifetime, configure);
-        collection.ConfigureAzureFunctions(builder);
-        return builder;
+        builder.UseFunctionContextAccessor();
+        var handlerBuilder = builder.Services.AddWhatsAppCore(handler, lifetime, configure);
+        return ConfigureAzureFunctions(builder.Services, handlerBuilder);
     }
 
     /// <summary>
     /// Configure the WhatsApp handler with an anonymous handler delegate that receives a service for Azure Functions hosting.
     /// </summary>
-    [EditorBrowsable(EditorBrowsableState.Never)]
     public static WhatsAppHandlerBuilder AddWhatsApp<TService>(
-        this IServiceCollection collection,
+        this IFunctionsWorkerApplicationBuilder builder,
         Func<TService, IEnumerable<IMessage>, CancellationToken, IAsyncEnumerable<Response>> handler,
         ServiceLifetime lifetime = ServiceLifetime.Singleton,
         Action<WhatsAppOptions>? configure = null)
         where TService : notnull
     {
-        var builder = collection.AddWhatsAppCore(handler, lifetime, configure);
-        collection.ConfigureAzureFunctions(builder);
-        return builder;
+        builder.UseFunctionContextAccessor();
+        var handlerBuilder = builder.Services.AddWhatsAppCore(handler, lifetime, configure);
+        return ConfigureAzureFunctions(builder.Services, handlerBuilder);
     }
 
     /// <summary>
     /// Configure the WhatsApp handler with an anonymous handler delegate that receives two services for Azure Functions hosting.
     /// </summary>
-    [EditorBrowsable(EditorBrowsableState.Never)]
     public static WhatsAppHandlerBuilder AddWhatsApp<TService1, TService2>(
-        this IServiceCollection collection,
+        this IFunctionsWorkerApplicationBuilder builder,
         Func<TService1, TService2, IEnumerable<IMessage>, CancellationToken, IAsyncEnumerable<Response>> handler,
         ServiceLifetime lifetime = ServiceLifetime.Singleton,
         Action<WhatsAppOptions>? configure = null)
         where TService1 : notnull
         where TService2 : notnull
     {
-        var builder = collection.AddWhatsAppCore(handler, lifetime, configure);
-        collection.ConfigureAzureFunctions(builder);
-        return builder;
+        builder.UseFunctionContextAccessor();
+        var handlerBuilder = builder.Services.AddWhatsAppCore(handler, lifetime, configure);
+        return ConfigureAzureFunctions(builder.Services, handlerBuilder);
     }
 
     /// <summary>
     /// Configure the WhatsApp handler with an anonymous handler delegate that receives three services for Azure Functions hosting.
     /// </summary>
-    [EditorBrowsable(EditorBrowsableState.Never)]
     public static WhatsAppHandlerBuilder AddWhatsApp<TService1, TService2, TService3>(
-        this IServiceCollection collection,
+        this IFunctionsWorkerApplicationBuilder builder,
         Func<TService1, TService2, TService3, IEnumerable<IMessage>, CancellationToken, IAsyncEnumerable<Response>> handler,
         ServiceLifetime lifetime = ServiceLifetime.Singleton,
         Action<WhatsAppOptions>? configure = null)
@@ -150,17 +145,16 @@ public static class WhatsAppFunctionsServiceCollectionExtensions
         where TService2 : notnull
         where TService3 : notnull
     {
-        var builder = collection.AddWhatsAppCore(handler, lifetime, configure);
-        collection.ConfigureAzureFunctions(builder);
-        return builder;
+        builder.UseFunctionContextAccessor();
+        var handlerBuilder = builder.Services.AddWhatsAppCore(handler, lifetime, configure);
+        return ConfigureAzureFunctions(builder.Services, handlerBuilder);
     }
 
     /// <summary>
     /// Configure the WhatsApp handler with an anonymous handler delegate that receives four services for Azure Functions hosting.
     /// </summary>
-    [EditorBrowsable(EditorBrowsableState.Never)]
     public static WhatsAppHandlerBuilder AddWhatsApp<TService1, TService2, TService3, TService4>(
-        this IServiceCollection collection,
+        this IFunctionsWorkerApplicationBuilder builder,
         Func<TService1, TService2, TService3, TService4, IEnumerable<IMessage>, CancellationToken, IAsyncEnumerable<Response>> handler,
         ServiceLifetime lifetime = ServiceLifetime.Singleton,
         Action<WhatsAppOptions>? configure = null)
@@ -169,17 +163,16 @@ public static class WhatsAppFunctionsServiceCollectionExtensions
         where TService3 : notnull
         where TService4 : notnull
     {
-        var builder = collection.AddWhatsAppCore(handler, lifetime, configure);
-        collection.ConfigureAzureFunctions(builder);
-        return builder;
+        builder.UseFunctionContextAccessor();
+        var handlerBuilder = builder.Services.AddWhatsAppCore(handler, lifetime, configure);
+        return ConfigureAzureFunctions(builder.Services, handlerBuilder);
     }
 
     /// <summary>
     /// Configure the WhatsApp handler with an anonymous handler delegate that receives five services for Azure Functions hosting.
     /// </summary>
-    [EditorBrowsable(EditorBrowsableState.Never)]
     public static WhatsAppHandlerBuilder AddWhatsApp<TService1, TService2, TService3, TService4, TService5>(
-        this IServiceCollection collection,
+        this IFunctionsWorkerApplicationBuilder builder,
         Func<TService1, TService2, TService3, TService4, TService5, IEnumerable<IMessage>, CancellationToken, IAsyncEnumerable<Response>> handler,
         ServiceLifetime lifetime = ServiceLifetime.Singleton,
         Action<WhatsAppOptions>? configure = null)
@@ -189,17 +182,16 @@ public static class WhatsAppFunctionsServiceCollectionExtensions
         where TService4 : notnull
         where TService5 : notnull
     {
-        var builder = collection.AddWhatsAppCore(handler, lifetime, configure);
-        collection.ConfigureAzureFunctions(builder);
-        return builder;
+        builder.UseFunctionContextAccessor();
+        var handlerBuilder = builder.Services.AddWhatsAppCore(handler, lifetime, configure);
+        return ConfigureAzureFunctions(builder.Services, handlerBuilder);
     }
 
     /// <summary>
     /// Configure the WhatsApp handler with an anonymous handler delegate that receives six services for Azure Functions hosting.
     /// </summary>
-    [EditorBrowsable(EditorBrowsableState.Never)]
     public static WhatsAppHandlerBuilder AddWhatsApp<TService1, TService2, TService3, TService4, TService5, TService6>(
-        this IServiceCollection collection,
+        this IFunctionsWorkerApplicationBuilder builder,
         Func<TService1, TService2, TService3, TService4, TService5, TService6, IEnumerable<IMessage>, CancellationToken, IAsyncEnumerable<Response>> handler,
         ServiceLifetime lifetime = ServiceLifetime.Singleton,
         Action<WhatsAppOptions>? configure = null)
@@ -210,8 +202,40 @@ public static class WhatsAppFunctionsServiceCollectionExtensions
         where TService5 : notnull
         where TService6 : notnull
     {
-        var builder = collection.AddWhatsAppCore(handler, lifetime, configure);
-        collection.ConfigureAzureFunctions(builder);
-        return builder;
+        builder.UseFunctionContextAccessor();
+        var handlerBuilder = builder.Services.AddWhatsAppCore(handler, lifetime, configure);
+        return ConfigureAzureFunctions(builder.Services, handlerBuilder);
+    }
+
+    internal static WhatsAppHandlerBuilder ConfigureAzureFunctions(this IServiceCollection services, WhatsAppHandlerBuilder handlerBuilder, Action<QueueClientOptions>? configure = null)
+    {
+        // Remove the default handler and runner factories from the core registration
+        var handlerFactory = services.FirstOrDefault(x => x.ServiceType == typeof(Func<IWhatsAppHandler>));
+        if (handlerFactory != null)
+            services.Remove(handlerFactory);
+
+        var runnerFactory = services.FirstOrDefault(x => x.ServiceType == typeof(Func<PipelineRunner>));
+        if (runnerFactory != null)
+            services.Remove(runnerFactory);
+
+        // Register Azure Functions-specific handler and runner factories that use FunctionContext
+        services.AddSingleton<Func<IWhatsAppHandler>>(services => () =>
+        {
+            var accessor = services.GetRequiredService<IFunctionContextAccessor>();
+            var ctx = accessor.FunctionContext ?? throw new InvalidOperationException("FunctionContext is not available.");
+            return ctx.InstanceServices.GetRequiredService<IWhatsAppHandler>();
+        });
+
+        services.AddSingleton<Func<PipelineRunner>>(services => () =>
+        {
+            var accessor = services.GetRequiredService<IFunctionContextAccessor>();
+            var ctx = accessor.FunctionContext ?? throw new InvalidOperationException("FunctionContext is not available.");
+            return ctx.InstanceServices.GetRequiredService<PipelineRunner>();
+        });
+
+        // Register the queue processor as the default message processor for Azure Functions
+        handlerBuilder.UseQueueProcessor(configure);
+
+        return handlerBuilder;
     }
 }
